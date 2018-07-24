d594ca3e1f847259868067ce41417893f993e2ad
Rémy Léone <rleone@online.net>
2018-07-24 16:55:11 +0200

    Add a multi provider example

---
+++ code/terraform/example07/README.md
@@ -0,0 +1,10 @@
+---
+layout: default
+---
+
+Example using different providers
+
+    terraform init
+    terraform plan
+    terraform apply
+    terraform destroy
---
+++ code/terraform/example07/main.tf
@@ -0,0 +1,34 @@
+provider "scaleway" {
+  region = "par1"
+}
+
+provider "scaleway" {
+  region = "ams1"
+  alias  = "amsterdam"
+}
+
+data "scaleway_image" "par" {
+  architecture = "x86_64"
+  name         = "Ubuntu Bionic"
+}
+
+data "scaleway_image" "ams" {
+  architecture = "x86_64"
+  name         = "Ubuntu Bionic"
+  provider = "scaleway.amsterdam"
+}
+
+resource "scaleway_server" "example07_par" {
+  name  = "example07-paris"
+  image = "${data.scaleway_image.par.id}"
+  type  = "C2S"
+  state = "stopped"
+}
+
+resource "scaleway_server" "example07_ams" {
+  name     = "example07-amsterdam"
+  image    = "${data.scaleway_image.ams.id}"
+  type     = "C2S"
+  state = "stopped"
+  provider = "scaleway.amsterdam"
+}
