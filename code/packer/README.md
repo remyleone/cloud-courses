35f77516bc8f1400c70b12cd2688404c1bc7bbb0
Rémy Léone <rleone@online.net>
2018-09-21 16:30:04 +0200

    Use packer to build a cloud-courses machine

--- code/packer/README.md
+++ code/packer/README.md
@@ -6,7 +6,7 @@ permalink: /code/packer
 Example using Packer.
 
 {% highlight json %}
-{% include_relative scaleway-odoo-template.json %}
+{% include_relative scaleway-cloud-courses-template.json %}
 {% endhighlight %}
 
 {% highlight bash %}
--- code/packer/ubuntu_install.sh
+++ code/packer/ubuntu_install.sh
@@ -6,9 +6,46 @@ export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
 tzdata tzdata/Areas select Europe
 tzdata tzdata/Zones/Europe select Paris
 
-apt-get update
-apt-get install postgresql -y
-apt-get install wkhtmltopdf
-wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
-echo "deb http://nightly.odoo.com/11.0/nightly/deb/ ./" >> /etc/apt/sources.list.d/odoo.list
-apt-get update && apt-get install odoo -y
+# Install system-wide packages
+
+apt update
+apt dist-upgrade -y
+apt install -y git-core vim-nox htop tree tmux zsh curl python-pip python3-pip golang sudo
+
+# Install Docker-CE
+
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt update
+apt install -y docker-ce
+docker run hello-world
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sed -i s/robbyrussell/ys/ .zshrc
+echo 'export PATH=$HOME/go/bin:$PATH' >> $HOME/.zshrc
+
+# Install Packer
+
+go get -v github.com/hashicorp/packer
+
+# Install Terraform
+
+go get -v github.com/hashicorp/terraform
+
+# Install ansible
+
+python3 -m pip install ansible
+
+# Install the courses content
+
+git clone https://github.com/scaleway/cloud-courses
+
